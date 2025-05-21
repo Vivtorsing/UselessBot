6121ac31c94f96d536bd2b3adc6e7e57b7f3a6aa
Vivtorsing <43187711+Vivtorsing@users.noreply.github.com>
2025-05-21 12:21:36 -0400

    Added Pre Release 2 and 3

--- src/Commands.js
+++ src/Commands.js
@@ -41,114 +41,58 @@ function AnimatedSection({ children, direction = "left" }) {
 }
 
 //all the commands
-/*const commandCategories = {
-  Help: [
-    {name: "/help (section)", description: "Where you get a list of Useless Bot's Commands"},
-    {name: "/server", description: "Where you can join Useless Bot's server"},
-    {name: "/invite", description: "Where you can invite Useless Bot to your server"},
-    {name: "/vote", description: "Where you can vote for Useless Bot and get some perks"}
-  ],
-  Weather: [
-    {name: "/weather (location)", description: "Where you can get the weather for that location"}
-  ],
-  AI: [
-    {name: "/ai chat (message)", description: "Where you can talk to Ucy AI"},
-    {name: "/ai image (text)", description: "Where you can generate an AI image"}
-  ],
-  Anime: [
-    {name: "/anime (type)", description: "Where you can get some anime"},
-    {name: "/animegif (type)", description: "Where you can get some anime gifs"},
-  ],
-  Joke: [
-    {name: "/joke (type)", description: "Where you can get some jokes"}
-  ],
-  Meme: [
-    {name: "/meme (type)", description: "Where you can get some memes"}
-  ],
-  Economy: [
-    {name: "/register", description: "Where you can register and start using economy commands"},
-    {name: "/daily", description: "Where you can get your daily"},
-    {name: "/shop (business)", description: "Where you can buy stuff"},
-    {name: "/balance (user)", description: "Where you can check your or others balance"},
-    {name: "/stats (user)", description: "Where you can check your stats"},
-    {name: "/leaderboard (type)", description: "Where you can check the global or local leaderboard"},
-    {name: "/work", description: "Where you can work for money"},
-  ],
-  Other: [
-    {name: "/minesweeper", description: "Where you can get a minesweeper game"},
-    {name: "/rps (choice)", description: "Where you can play rock paper scissors against Useless Bot"},
-    {name: "/slots", description: "Where you can play slots"},
-    {name: "/8ball (question)", description: "Where you can get a question and get a response"},
-    {name: "/magicconch (question)", description: "Where the Magic Conch Shell answers your question"},
-    {name: "/love (user)", description: "Where Useless Bot predicts how much you love a certain user"},
-    {name: "/poll (question)", description: "Where you can create a poll"},
-    {name: "/say (statement)", description: "Where you can make Useless Bot say something"},
-    {name: "/code (message)", description: "Where you can encode your message"},
-    {name: "/minecraft player (player)", description: "Where you can get some information on a Minecraft player"},
-    {name: "/minecraft server (server ip)", description: "Where you can get some information about a Minecraft server"},
-    {name: "/word", description: "Where you can learn a new word"},
-    {name: "/fruit", description: "Where Useless Bot gives you a random fruit"},
-    {name: "/card", description: "Where you can get a random card"},
-    {name: "/ping", description: "Where you can check Useless Bot Ping"},
-    {name: "icon (user)", description: "Where you can get a user's icon"},
-    {name: "/status", description: "Where you can check Useless Bot's status"},
-    {name: "/time (timezone)", description: "Where you can check the time in a certain timezone"},
-    {name: "/version", description: "Where you can check Useless Bot's version"},
-    {name: "/report", description: "Where you can report bugs or request new features"},
-    {name: "/reddit (reddit)", description: "No longer worker due to Reddit new terms"},
-    {name: "/food", description: "Might no longer be working..."},
-  ]
-};*/
 const commandCategories = {
   Main: [
-      {name: "/botstatus", description: "Where you can check the status of Useless Bot"},
-      {name: "/donate", description: "The place where you can donate and get premium features"},
-      {name: "/feedback", description: "To report bugs, or ask for a new command"},
-      {name: "/help (section)", description: "Where you get a list of Useless Bot's Commands"},
-      {name: "/invite", description: "Where you can invite Useless Bot to your server"},
-      {name: "/server", description: "Where you can join Useless Bot's server"},
-      {name: "/version", description: "To check Useless Bot's version"},
-      {name: "/vote", description: "Where you can vote for Useless Bot and get some perks"}
+    { name: "/botstatus", description: "Where you can check the status of Useless Bot" },
+    { name: "/donate", description: "The place where you can donate and get premium features" },
+    { name: "/feedback", description: "To report bugs, or ask for a new command" },
+    { name: "/help (section)", description: "Where you get a list of Useless Bot's Commands" },
+    { name: "/invite", description: "Where you can invite Useless Bot to your server" },
+    { name: "/server", description: "Where you can join Useless Bot's server" },
+    { name: "/version", description: "To check Useless Bot's version" },
+    { name: "/vote", description: "Where you can vote for Useless Bot and get some perks" }
   ],
   Fun: [
-      {name: "/ai chat (message)", description: "Where you can talk to Ucy AI"},
-      {name: "/ai image (text)", description: "Where you can generate an AI image"},
-      {name: "/ai speech (message)", description: "Where Ucy AI talks instead of writing"},
-      {name: "/anime (type)", description: "Where you can get some anime"},
-      {name: "/animegif (type)", description: "Where you can get some anime gifs"},
-      {name: "/joke (type)", description: "Where you can get some jokes"},
-      {name: "/meme (type)", description: "Where you can get some memes"},
-      {name: "/rps (choice)", description: "Where you can play rock paper scissors against Useless Bot"},
-      {name: "/slots", description: "Where you can play slots"}
+    { name: "/ai chat (message)", description: "Where you can talk to Ucy AI" },
+    { name: "/ai image (text)", description: "Where you can generate an AI image" },
+    { name: "/ai speech (message)", description: "Where Ucy AI talks instead of writing" },
+    { name: "/ai search (message)", description: "Where Ucy AI search the internet before answering your question" },
+    { name: "/anime (type)", description: "Where you can get some anime" },
+    { name: "/animegif (type)", description: "Where you can get some anime gifs" },
+    { name: "/joke (type)", description: "Where you can get some jokes" },
+    { name: "/meme (type)", description: "Where you can get some memes" },
+    { name: "/rps (choice)", description: "Where you can play rock paper scissors against Useless Bot" },
+    { name: "/slots", description: "Where you can play slots" }
   ],
   Useful: [
-      {name: "/crypto (name)", description: "Where you can get crypto price and other information"},
-      {name: "/minecraft player (player)", description: "Where you can get some information on a Minecraft player"},
-      {name: "/minecraft server (server ip)", description: "Where you can get some information about a Minecraft server"},
-      {name: "/poll (question)", description: "Where you can create a poll"},
-      {name: "/stock (name)", description: "Where you can get stock information"},
-      {name: "/weather (location)", description: "Get your weather forecast"},
-      {name: "/word", description: "Learn a new word"}
+    { name: "/crypto (name)", description: "Where you can get crypto price and other information" },
+    { name: "/minecraft player (player)", description: "Where you can get some information on a Minecraft player" },
+    { name: "/minecraft server (server ip)", description: "Where you can get some information about a Minecraft server" },
+    { name: "/poll (question)", description: "Where you can create a poll" },
+    { name: "/stock (name)", description: "Where you can get stock information" },
+    { name: "/weather (location)", description: "Get your weather forecast" },
+    { name: "/word", description: "Learn a new word" }
   ],
   Silly: [
-      {name: "/birthday", description: "Where you can tell Useless Bot Happy Birthday"},
-      {name: "/card", description: "Where you can get a random card"},
-      {name: "/code (encode/decode) (message)", description: "Where you can encode or decode your message"},
-      {name: "/8ball (question)", description: "Where you can get a question and get a response"},
-      {name: "/fruit", description: "Where Useless Bot gives you a random fruit"},
-      {name: "icon (user)", description: "Where you can get a user's icon"},
-      {name: "/love (user)", description: "Where Useless Bot predicts how much you love a certain user"},
-      {name: "/magicconch (question)", description: "Where the Magic Conch Shell answers your question"},
-      {name: "/minesweeper", description: "Where you can get a minesweeper game"}
+    { name: "/birthday", description: "Where you can tell Useless Bot Happy Birthday" },
+    { name: "/card", description: "Where you can get a random card" },
+    { name: "/code (encode/decode) (message)", description: "Where you can encode or decode your message" },
+    { name: "/8ball (question)", description: "Where you can get a question and get a response" },
+    { name: "/fruit", description: "Where Useless Bot gives you a random fruit" },
+    { name: "icon (user)", description: "Where you can get a user's icon" },
+    { name: "/love (user)", description: "Where Useless Bot predicts how much you love a certain user" },
+    { name: "/magicconch (question)", description: "Where the Magic Conch Shell answers your question" },
+    { name: "/minesweeper", description: "Where you can get a minesweeper game" }
   ],
   Economy: [
-      {name: "/balance (user)", description: "Where you can check your or others balance"},
-      {name: "/daily", description: "Where you can get your daily"},
-      {name: "/leaderboard", description: "Check the top economy players"},
-      {name: "/register", description: "Use this command to use economy commands and some other commands"},
-      {name: "/shop (business)", description: "Where you can buy stuff"},
-      {name: "/stats (user)", description: "Where you can check your stats"},
-      {name: "/work", description: "Where you can work for money"}
+    { name: "/balance (user)", description: "Where you can check your or others balance" },
+    { name: "/daily", description: "Where you can get your daily" },
+    { name: "/leaderboard", description: "Check the top economy players" },
+    { name: "/register", description: "Use this command to use economy commands and some other commands" },
+    { name: "/shop (business)", description: "Where you can buy stuff" },
+    { name: "/stats (user)", description: "Where you can check your stats" },
+    { name: "/upgrade (upgrade)", description: "Where you can buy upgrades for your shops" },
+    { name: "/work", description: "Where you can work for money" }
   ]
 }
 
@@ -187,10 +131,10 @@ export default function Commands() {
               {/*command*/}
               {commands.map((command, index) => (
                 <section className="commandSection">
-                    <AnimatedSection key={command.name} direction={index % 2 === 0 ? "left" : "right"}>
+                  <AnimatedSection key={command.name} direction={index % 2 === 0 ? "left" : "right"}>
                     <h3 className="text-2xl font-bold text-pink-400">{command.name}</h3>
                     <p className="text-gray-300 mt-2">{command.description}</p>
-                    </AnimatedSection>
+                  </AnimatedSection>
                 </section>
               ))}
             </div>
--- src/releaseNotesData.js
+++ src/releaseNotesData.js
@@ -1,56 +1,95 @@
+import { version } from "react";
+import { data } from "react-router-dom";
+
 const releaseNotes = [
-    {
-      version: "v3.0",
-      date: "April 29, 2025",
-      sections: {
-        "Massive Changes": [
-          "Achievement System! You can now get achievement for using commands! Currently 4 but more to come!",
-          "Voting for Useless Bot is automatically dectected",
-          "New economy system to allow better shops and new things in the future (sadly everyone progress have been deleted but now it is easier to catch up!)",
-          "/ai chat now uses OpenAI's ChatGPT model 40",
-          "/ai speech a new command that is like /ai chat but uses OpenAI's voice models to speak instead of reading! (Good for bedtime stories)",
-          "/slots now has animations!",
-          "/weather has nicer interface for premium users (free users still has some work left to do...)"
-        ],
-        "Big Changes": [
-          "/code now has encode and decode options to encrypt or decrypt your code!",
-          "/crypto now uses a better source for more accurate crypto data",
-          "/joke has a new joke source for new jokes!",
-          "/meme now has a new source for fresh memes!",
-          "/stock has a nicer interface"
-        ],
-        "Small Changes": [
-          "/card now gives you a card emoji thing",
-          "/help command is nicer and easier to update (update part is for me lol)",
-          "/minesweeper is more efficient and loads faster"
-        ],
-        "Removed Commands": [
-          "Removed food command",
-          "Removed music command",
-          "Removed ping command",
-          "Removed reddit command",
-          "Removed say command",
-          "Removed search command (might add /ai search in the future)",
-          "Removed time command"
-        ],
-        "Bug Fixes": [
-          "Fixed ~22 bugs that can crash Useless Bot",
-          "Fixed too many smaller bugs that could cause skips and break stuff...",
-          "Filter for some commands were not working now fixed"
-        ],
-        "Notes": [
-          "AI commands now use a credit system and free users have 10 credits but voting will give an additional 90 credits for free! Also Premium users have unlimited credits!",
-          "Everyone's save has been reset. Apologies for this but we had no choice as our old system is not compatable with our newer more efficient version!"
-        ]
-      }
-    },
-    {
-      version: "I am too lazy to write more lol",
-      date: "NA",
-      sections: {
-        
-      }
+  {
+    version: "V3.0 Pre Release 3",
+    date: "May 20, 2025",
+    sections: {
+      "Big Changes": [
+        "/upgrade - now has over 30+ new upgrades for all your shops!!! (I wanted to make some of them funny but I only think that 1 of them are). Also upgrades will no longer show the upgrades that you already bought!"
+      ],
+      "Bug Fixes": [
+        "Fixed leaderboard not being in order",
+        "Fixed rounding issue in daily and work",
+        "Fixed 4 commands with Crash potential!"
+      ]
     }
+  },
+  {
+    version: "V3.0 Pre Release 2",
+    date: "May 13, 2025",
+    sections: {
+      "New": [
+        "/ai search (message) - now Ucy AI has access to the internet with the help of OpenAI ChatGPT newest search model!",
+        "/upgrade (upgrade) - we added upgrades!!!!! Now economy players can upgrade their shops to make them cheaper or more profitable!!!! There are currently 4 for now and will be add more slowly! Current plan is to have at least 2 upgrades for every shop!"
+      ],
+      "Smaller Things": [
+        "Weather command has time fixed for hourly and also added weather emojis to make the UI look nicer!",
+        "Leaderboard has an Xp leaderboard now. Xp is kinda useless right now even though we been having it for years now but I guess this will do for now...",
+        "Help command has a link to Useless Bot's website where you can view all the commands there as well!"
+      ],
+      "Bug Fixes": [
+        "Xp was not being given to players that use commands",
+        "Ping logging issue (only affected me lol)",
+        "Some link buttons were wrong...",
+        "There are more which I did not write down..."
+      ]
+
+    }
+  },
+  {
+    version: "V3.0 Pre Release 1",
+    date: "April 29, 2025",
+    sections: {
+      "Massive Changes": [
+        "Achievement System! You can now get achievement for using commands! Currently 4 but more to come!",
+        "Voting for Useless Bot is automatically dectected",
+        "New economy system to allow better shops and new things in the future (sadly everyone progress have been deleted but now it is easier to catch up!)",
+        "/ai chat now uses OpenAI's ChatGPT model 40",
+        "/ai speech a new command that is like /ai chat but uses OpenAI's voice models to speak instead of reading! (Good for bedtime stories)",
+        "/slots now has animations!",
+        "/weather has nicer interface for premium users (free users still has some work left to do...)"
+      ],
+      "Big Changes": [
+        "/code now has encode and decode options to encrypt or decrypt your code!",
+        "/crypto now uses a better source for more accurate crypto data",
+        "/joke has a new joke source for new jokes!",
+        "/meme now has a new source for fresh memes!",
+        "/stock has a nicer interface"
+      ],
+      "Small Changes": [
+        "/card now gives you a card emoji thing",
+        "/help command is nicer and easier to update (update part is for me lol)",
+        "/minesweeper is more efficient and loads faster"
+      ],
+      "Removed Commands": [
+        "Removed food command",
+        "Removed music command",
+        "Removed ping command",
+        "Removed reddit command",
+        "Removed say command",
+        "Removed search command (might add /ai search in the future)",
+        "Removed time command"
+      ],
+      "Bug Fixes": [
+        "Fixed ~22 bugs that can crash Useless Bot",
+        "Fixed too many smaller bugs that could cause skips and break stuff...",
+        "Filter for some commands were not working now fixed"
+      ],
+      "Notes": [
+        "AI commands now use a credit system and free users have 10 credits but voting will give an additional 90 credits for free! Also Premium users have unlimited credits!",
+        "Everyone's save has been reset. Apologies for this but we had no choice as our old system is not compatable with our newer more efficient version!"
+      ]
+    }
+  },
+  {
+    version: "I am too lazy to write more lol",
+    date: "NA",
+    sections: {
+
+    }
+  }
 ];
-  
+
 export default releaseNotes;  
